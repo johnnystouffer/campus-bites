92b8c4d7ec6ca24e2b3043f837fd43834b9b033a
johnnystouffer <stouff17@msu.edu>
2024-09-28 18:20:57 -0400

    finsihing the layout

--- frontend/src/assets/pages/Register.jsx
+++ frontend/src/assets/pages/Register.jsx
@@ -10,10 +10,11 @@ function Register() {
   // Handler for form submission
   const handleSubmit = (e) => {
     e.preventDefault();
+
+
     // Implement your registration logic here
-    console.log('Username:', username);
-    console.log('Email:', email);
-    console.log('Password:', password);
+
+
     // Clear form fields after submission
     setUsername('');
     setEmail('');
@@ -21,35 +22,37 @@ function Register() {
   };
 
   return (
+    <div className='overall-container'>
     <div className="register-container">
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text"
-          placeholder="Enter Username"
-          name="username"
-          required
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input 
-          type="email"
-          placeholder="Enter Email"
-          name="email"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input 
-          type="password"
-          placeholder="Enter Password"
-          name="password"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Register</button>
-      </form>
+        <h2>Register</h2>
+        <form className="form-container" onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Register</button>
+        </form>
+        </div>
     </div>
   );
 }
